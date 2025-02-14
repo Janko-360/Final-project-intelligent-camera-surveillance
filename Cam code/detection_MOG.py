ba3cfb65cafb22adb1f0de6e842b8e7c0ed16b7f
Janko-360 <jankobauer77@gmail.com>
2025-02-14 14:21:50 +0200

    Fine tuned recording triggers and stops.

--- Cam code/detection_MOG.py
+++ Cam code/detection_MOG.py
@@ -1,13 +1,17 @@
 import cv2
 import numpy as np
 from collections import deque
-from time import sleep
+from time import sleep, time
 import requests 
+import subprocess
 
 
 video = cv2.VideoCapture(0)
 width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
 height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
+
+max_video_limit = 10
+min_video_limit = 2
 
 
 def paint_contours(mask, frame): 
@@ -29,15 +33,39 @@ def paint_contours(mask, frame):
 
 url_old = 'http://192.168.100.15:5000/api'
 url = 'http://127.0.0.1:5000/api'
+
+def get_length(vid_name):
+    '''Returns the length of the video in seconds''' 
+    cmd = f'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "{vid_name}"'
+    res = None
+    try: 
+        res = subprocess.check_output(cmd, shell=True)
+        res = str(res) 
+        res = float(res[2:7])
+    except: 
+        print(f'Error getting video: {res}')
+        res = None
+    return res         
+
 def upload_video(video_name): 
     '''Http upload to the local server'''
-    r = 0
-    with open(video_name, 'rb') as f: 
-        r = requests.post(url, files={'file': f})
-    if r.status_code == 200:
-        print('Good video upload')
+    # Only upload the video if it is long enough
+    if get_length(video_name) > min_video_limit: 
+        r = 0
+        with open(video_name, 'rb') as f: 
+            r = requests.post(url, files={'file': f})
+        if r.status_code == 200:
+            print('Good video upload')
+        else: 
+            print(f"Upload error response code: {r.status_code}")
     else: 
-        print(f"Upload error response code: {r.status_code}")
+        # Remove video and don't upload 
+        cmd = f'del ".\\videos\\{video_name}"'
+        try: 
+            subprocess.run(cmd, shell=True)
+        except subprocess.CalledProcessError as err: 
+            print('Subprocess error in "Upload video"')
+            print(f'error code: {err.returncode}')
 
 # Initialize the background objects.
 bgObj1 = cv2.bgsegm.createBackgroundSubtractorMOG()
@@ -51,10 +79,11 @@ kernel = np.ones((kern_size, kern_size), np.float32)/25
 sens_thresh_start = 0.25 # sense thresh as % 
 sens_thresh_end = sens_thresh_start * 2
 sens_old = 0
-act_delay = 20 # history, number of frames
+act_delay = 30 # history, number of frames
 # Activity history is a queue, latest frame average gets pushed and the oldest gets dropped 
 activity_hist = deque(act_delay*[0], act_delay)
 
+# False if not recording, if recording: float time stamp as start of recording 
 recording = False
 
 # video writer/saver settings
@@ -66,6 +95,7 @@ fourcc = cv2.VideoWriter_fourcc(*'XVID')
 while True:    
     # Read a new frame.
     ret, frame = video.read()
+    frame = cv2.resize(frame, (640, 480))
 
     # Check if frame is not read correctly.
     if not ret:
@@ -85,27 +115,34 @@ while True:
 
     if act_now > np.mean(activity_hist)*(1+sens_thresh_start) and recording == False:
         # show activity & start recording 
-        print(f'Starting recording: act now {round(act_now, 2)}, mean act hist {round(np.mean(activity_hist))}')
-        recording = True
+        # print(f'Starting recording: act now {round(act_now, 2)}, mean act hist {round(np.mean(activity_hist))}')
+        recording = time()
         # Create new video file to save frames to
         video_count += 1
         video_name = f'.\\videos\\output{video_count}.avi'
         out = cv2.VideoWriter(video_name, fourcc, 20.0, (width, height))
-    elif act_now >=  np.mean(activity_hist)*(1-sens_thresh_start) and recording == True: # add lower tolerance (recording will not stop in case of activity spikes or if it slows down a bit.)
+    elif act_now >=  np.mean(activity_hist)*(1-sens_thresh_start) and type(recording) == float: # add lower tolerance (recording will not stop in case of activity spikes or if it slows down a bit.)
         # record, there is activity 
-        print(f'Recording: act now {round(act_now)}, mean act hist {round(np.mean(activity_hist))}')
+        # print(f'Recording: act now {round(act_now)}, mean act hist {round(np.mean(activity_hist))}')
         out.write(frame)
-        cv2.imshow('Recording', frame)
-    elif act_now < np.mean(activity_hist)*(1-sens_thresh_end) and recording == True: # If activity is lower than the average (things really slow down or stop), then stop recording 
+        cv2.imshow('Recording', frame)    
+    elif act_now < np.mean(activity_hist)*(1-sens_thresh_end) and type(recording) == float: # If activity is lower than the average (things really slow down or stop), then stop recording 
         # stop recording, activity slowed or stopped/finished
-        print(f'Stopping recording: act now {round(act_now)}, mean act hist {round(np.mean(activity_hist))}')
+        # print(f'Stopping recording: act now {round(act_now)}, mean act hist {round(np.mean(activity_hist))}')
         recording = False
         out.release() 
         cv2.destroyWindow('Recording')
-        # upload_video(video_name)
+        # if vid_length long enough: 
+            # upload_video(video_name)
+        # else: 
+            # delete vid 
     else: # do nothing since there is no activity to record. 
         pass
 
+    # Stop recording if old enough (after 5s)
+    if recording and time.time()-recording > max_video_limit:
+        print('Stop: recording is to old')
+        recording = False
 
     k = cv2.waitKey(1) & 0xff
     # Quit when a key is pressed.
